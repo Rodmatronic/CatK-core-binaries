e6f5020b98ca77f320111cee488bf37de02dfbf5
Rodmatronic <roderickmpike@icloud.com>
2024-07-08 15:59:34 -0300

    Wrote simple sync coreutil

---
+++ corebin/sync.c
@@ -0,0 +1,12 @@
+
+// sync - CatK system binary
+// Written by Rodmatronics
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    // Call the sync system call to flush filesystem buffers
+    sync();
+    return 0;
+}
