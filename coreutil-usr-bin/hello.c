f2cc8f18fba12f38300148ab7f3fcd5b92b2bb27
Rodmatronic <roderickmpike@icloud.com>
2024-07-29 20:50:22 -0300

    Added test program for Syscalls / C execution in CatK

---
+++ coreutil-usr-bin/hello.c
@@ -0,0 +1,10 @@
+
+// hello - a Historic C program, from "The C Programming Language" book.
+// This is for testing if C/syscalls works on CatK.
+// Written by Brian W. Kernighan & Dennis M. Ritchie
+
+#include <stdio.h>
+main()
+{
+    printf("hello, world\n");
+}
